76cccf70bc5ecdcee59d5df3bac31e0091222fe9
bruno <bruno.lusardi.96@gmail.com>
2025-04-08 14:31:57 +0200

    refact prisma-client init + move logic in root prisma folder

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/prisma";
+import { prisma } from "../prisma/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
