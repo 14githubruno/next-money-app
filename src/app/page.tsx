1979c7758d749ca5b1ee0dc53cc3c6a1ceced60f
bruno <bruno.lusardi.96@gmail.com>
2025-04-09 14:01:51 +0200

    extract authconfig + refact middleware + add custom sign-in page + refact homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
-import { auth } from "@/auth";
-import Link from "next/link";
-import SignIn from "@/components/sign-in";
 import SignOut from "@/components/sign-out";
+import Link from "next/link";
+import { Fragment } from "react";
+import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
   const user = session?.user;
+  return (
+    <section>
+      <h1>Lorem Ipsum</h1>
+      {!user && <Link href={"/sign-in"}>Sign in</Link>}
 
-  if (user) {
-    return (
-      <div className="flex flex-col gap-2">
-        <Link href="/profile">I want to see my session info &rarr;</Link>
-        <SignOut />
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex flex-col gap-2">
-        <p>You are not signed in</p>
-        <SignIn />
-      </div>
-    );
-  }
+      {user && (
+        <Fragment>
+          <hr />
+          <SignOut />
+        </Fragment>
+      )}
+    </section>
+  );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import authConfig from "../auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../prisma/prisma";
 
@@ -8,7 +8,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
-  providers: [Google],
+  ...authConfig,
+  pages: {
+    signIn: "/sign-in",
+  },
   callbacks: {
     async session({ token, session }) {
       return {
