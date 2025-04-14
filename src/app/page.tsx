69c13d4b6c83da59f16d29228c4b4cc9791d6190
bruno <bruno.lusardi.96@gmail.com>
2025-04-14 15:20:51 +0200

    refact home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
-import SignOut from "@/components/sign-out";
 import Link from "next/link";
-import { Fragment } from "react";
 import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
   const user = session?.user;
   return (
-    <section>
-      <h1>Lorem Ipsum</h1>
+    <section className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl">Lorem Ipsum</h1>
       {!user && <Link href={"/sign-in"}>Sign in</Link>}
-
-      {user && (
-        <Fragment>
-          <hr />
-          <SignOut />
-        </Fragment>
-      )}
+      {user && <Link href={"/dashboard"}>Dashboard &rarr;</Link>}
     </section>
   );
 }
