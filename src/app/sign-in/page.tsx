c8cc5413ae1ab6455111573b5f0cdec6e6dbe2f0
bruno <bruno.lusardi.96@gmail.com>
2025-04-14 15:17:24 +0200

    refact sign-in form and page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import SignIn from "@/components/sign-in";
 
-export default async function Home() {
-  return <SignIn />;
+export default async function SignInPage() {
+  return (
+    <section className="flex h-screen flex-col items-center justify-center gap-4">
+      <SignIn />
+    </section>
+  );
 }
