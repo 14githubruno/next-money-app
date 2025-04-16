d5a6b9ffa921056341432790cd050ab664784375
bruno <bruno.lusardi.96@gmail.com>
2025-04-16 14:19:01 +0200

    init default categories if none

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "../auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../prisma/prisma";
+import { createDefaultCategories } from "./lib/queries/category";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -21,6 +22,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           id: token.sub, // pass to session.user MongoDB id
         },
       };
+    },
+
+    async signIn({ user }) {
+      const userId = user?.id;
+
+      if (userId) {
+        await createDefaultCategories(userId);
+      }
+
+      return true;
     },
   },
 });
