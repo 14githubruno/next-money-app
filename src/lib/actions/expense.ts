fee09693ba969672732daaf5f319dfc08f8b3ee5
bruno <bruno.lusardi.96@gmail.com>
2025-04-16 14:15:22 +0200

    add queries and server actions (for category and expense)

---
+++ src/lib/actions/expense.ts
@@ -0,0 +1,266 @@
+"use server";
+
+import prisma from "../../../prisma/prisma";
+import { expenseSchema } from "../validations/schemas";
+import { type ExpenseFormState } from "../types";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+
+/**
+ * This file contains all the expenses-related queries
+ * that ARE server actions.
+ *
+ * Other queries for expenses can be found here:
+ * - `src/lib/queries/expense.ts`
+ * =========================================================
+ */
+
+/**
+ * CREATE EXPENSE
+ * ========================================================
+ */
+export async function createExpense(
+  userId: string,
+  prevState: ExpenseFormState,
+  data: FormData
+): Promise<ExpenseFormState> {
+  let isSuccess: boolean = false;
+
+  const formData = Object.fromEntries(data);
+
+  // avoid type conflicts with FormData
+  const fieldValues = Object.assign(formData);
+
+  // validate form data
+  const validation = expenseSchema.safeParse(formData);
+
+  if (!validation.success) {
+    console.log("wrong validation", validation);
+    return {
+      success: false,
+      message: "Invalid data",
+      errors: validation.error.flatten().fieldErrors,
+      fieldValues,
+    };
+  }
+
+  // grab expense data
+  const expenseData = validation.data;
+
+  try {
+    // Verify the category exists and belongs to the user
+    const category = await prisma.category.findFirst({
+      where: {
+        id: expenseData.categoryId,
+        userId,
+      },
+    });
+
+    if (!category) {
+      throw new Error("Invalid category selected");
+    }
+
+    const newExpense = await prisma.expense.create({
+      data: {
+        ...expenseData,
+        userId,
+      },
+      include: {
+        category: true,
+      },
+    });
+
+    if (newExpense) {
+      isSuccess = true;
+      console.log(newExpense);
+    }
+
+    return {
+      success: true,
+      message: `Expense for category ${newExpense.category.name} created`,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        success: false,
+        message: error.message,
+        fieldValues,
+      };
+    }
+
+    throw new Error("Error creating expense");
+  } finally {
+    if (isSuccess) {
+      revalidatePath("/dashboard/expenses");
+      revalidatePath("/dashboard/categories");
+      redirect("/dashboard/expenses");
+    }
+  }
+}
+
+/**
+ * UPDATE EXPENSE
+ * ========================================================
+ */
+export async function updateExpense(
+  userId: string,
+  expenseId: string | undefined,
+  prevState: ExpenseFormState,
+  data: FormData
+): Promise<ExpenseFormState> {
+  let isSuccess: boolean = false;
+
+  const formData = Object.fromEntries(data);
+
+  // avoid type conflicts with FormData
+  const fieldValues = Object.assign(formData);
+
+  // validate form data
+  const validation = expenseSchema.safeParse(formData);
+
+  if (!validation.success) {
+    return {
+      success: false,
+      message: "Invalid data",
+      errors: validation.error.flatten().fieldErrors,
+      fieldValues,
+    };
+  }
+
+  // grab expense data
+  const expenseData = validation.data;
+
+  try {
+    const existingExpense = await prisma.expense.findUnique({
+      where: { id: expenseId, userId },
+    });
+
+    if (!existingExpense || existingExpense.userId !== userId) {
+      throw new Error(
+        "Expense not found or you do not have permission to update it"
+      );
+    }
+
+    const updatedExpense = await prisma.expense.update({
+      where: { id: expenseId, userId },
+      data: expenseData,
+      include: {
+        category: true,
+      },
+    });
+
+    if (updatedExpense) {
+      isSuccess = true;
+    }
+
+    return { success: true, message: "Expense updated" };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, message: error.message, fieldValues };
+    }
+
+    throw new Error("Error updating expense");
+  } finally {
+    if (isSuccess) {
+      revalidatePath("/dashboard/categories");
+      revalidatePath("/dashboard/expenses");
+      redirect("/dashboard/expenses");
+    }
+  }
+}
+
+/**
+ * DELETE EXPENSE
+ * ========================================================
+ */
+export async function deleteExpense(expenseId: string) {
+  const session = await auth();
+  const user = session?.user;
+  const userId = user?.id;
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  let isDeleted: boolean = false;
+
+  try {
+    const expense = await prisma.expense.findUnique({
+      where: { id: expenseId, userId },
+    });
+
+    if (!expense || expense.userId !== userId) {
+      throw new Error(
+        "Expense not found or you do not have permission to delete it"
+      );
+    }
+
+    const deletedExpense = await prisma.expense.delete({
+      where: { id: expenseId, userId },
+    });
+
+    if (deletedExpense) {
+      isDeleted = true;
+    }
+
+    return { success: true, message: "Expense deleted" };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, error: error.message };
+    }
+
+    throw new Error("Error in deleting the expense");
+  } finally {
+    if (isDeleted) {
+      revalidatePath("/dashboard/expenses");
+    }
+  }
+}
+
+/**
+ * JUST CONFIRM EXPENSE/PAYMENT
+ * ========================================================
+ */
+export async function confirmExpense(expenseId: string) {
+  const session = await auth();
+  const user = session?.user;
+  const userId = user?.id;
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  let isConfirmed: boolean = false;
+
+  try {
+    const expense = await prisma.expense.findUnique({
+      where: { id: expenseId, userId },
+    });
+
+    if (!expense || expense.userId !== userId) {
+      throw new Error(
+        "Expense not found or you do not have permission to delete it"
+      );
+    }
+
+    const confirmedExpense = await prisma.expense.update({
+      where: { id: expenseId, userId },
+      data: { isConfirmed: true },
+      include: { category: true },
+    });
+
+    if (confirmedExpense) {
+      isConfirmed = true;
+    }
+
+    return { success: true, message: "Expense confirmed" };
+  } catch (error) {
+    console.log("ERROR CONFIRMING EXPENSES: ", error);
+    throw new Error("Error confirming expense");
+  } finally {
+    if (isConfirmed) {
+      revalidatePath("/dashboard");
+    }
+  }
+}
---
+++ src/lib/queries/expense.ts
@@ -0,0 +1,102 @@
+import prisma from "../../../prisma/prisma";
+
+/**
+ * This file contains all the expenses-related
+ * that are NOT server actions.
+ *
+ * Server actions for expenses can be found here:
+ * - `src/lib/actions/expense.ts`
+ * =========================================================
+ */
+
+/**
+ * GET ALL USER EXPENSES
+ * ========================================================
+ */
+export async function getExpenses<T>(
+  userId: string | undefined,
+  filters?: Record<string, T>
+) {
+  try {
+    const userExpenses = await prisma.expense.findMany({
+      where: {
+        userId,
+        ...filters,
+      },
+      include: {
+        category: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return userExpenses;
+  } catch (error) {
+    console.error("ERROR EXPENSES: ", error);
+    throw new Error("Error fetching user expenses");
+  }
+}
+
+/**
+ * GET SINGLE EXPENSE
+ * ========================================================
+ */
+
+export async function getSingleExpense<T>(
+  expenseId: string,
+  userId: string | undefined,
+  toViewExpense = false,
+  filters?: Record<string, T>
+) {
+  try {
+    const expense = await prisma.expense.findUnique({
+      where: {
+        id: expenseId,
+        userId: userId!,
+        ...filters,
+      },
+
+      include: {
+        category: toViewExpense
+          ? {
+              select: {
+                name: true,
+                _count: true,
+              },
+            }
+          : true,
+      },
+    });
+
+    return expense;
+  } catch (error) {
+    console.error("ERROR SINGLE EXPENSE: ", error);
+    throw new Error("Error fetching single user expense");
+  }
+}
+
+/**
+ * GET TOTAL AMOUNT OF EXPENSES
+ * ========================================================
+ */
+export async function getTotalAmountExpenses<T>(
+  userId: string | undefined,
+  filters?: Record<string, T>
+) {
+  try {
+    const amount = await prisma.expense.aggregate({
+      where: {
+        userId,
+        ...filters,
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    return amount;
+  } catch (error) {
+    console.error("ERROR AGGREGATING: ", error);
+    throw new Error("Error aggregating amount expenses");
+  }
+}
