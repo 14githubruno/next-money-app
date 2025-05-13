c075bf72c95a653e09e30a1c2fd5e2d330ef89ff
bruno <bruno.lusardi.96@gmail.com>
2025-05-13 13:54:08 +0200

    add error-component

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -1,29 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { Button } from "@/components/tremor-raw/ui/button";
+import ErrorComponent from "@/components/global/error-component";
 
-type Props = {
+type ErrorProps = {
   error: Error & { digest?: string };
   reset: () => void;
 };
 
-/**
- *
- * @note Catch errors within dashboard
- */
-export default function Error({ error, reset }: Props) {
+export default function Error({ error, reset }: ErrorProps) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
-  return (
-    <section className="flex h-full flex-col items-center justify-center gap-4">
-      <h1 className="text-center">Something went wrong</h1>
-      <p>{error.message}</p>
-      <Button variant="base" onClick={() => reset()}>
-        Please try again
-      </Button>
-    </section>
-  );
+  return <ErrorComponent errorMessage={error.message} reset={reset} />;
 }
