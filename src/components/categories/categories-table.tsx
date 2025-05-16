9ac744642edb58be4eef014a5e60a484e5b43ba5
bruno <bruno.lusardi.96@gmail.com>
2025-05-16 19:46:52 +0200

    remove commented code from categories table

--- src/components/categories/categories-table.tsx
+++ src/components/categories/categories-table.tsx
@@ -89,9 +89,6 @@ export function CategoriesTable({
                 </TableCell>
                 <TableCell>
                   <div className="flex space-x-4">
-                    {/* <Link href={`/dashboard/categories/${category.id}`}>
-                      <Settings2 className="h-4 w-4" />
-                    </Link> */}
                     <CategoryForm category={category} isEditing={true} />
                     <DeleteDialog
                       deleteAction={() => deleteCurrentCategory(category)}
